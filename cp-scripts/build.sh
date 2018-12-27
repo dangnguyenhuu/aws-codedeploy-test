44b02fe2953776baa7fa5067418d55cd8341f06f
雪あすか <tt@kmycode.net>
2018-12-27 23:24:00 +0900

    シェルスクリプトを修正

--- cp-scripts/build.sh
+++ cp-scripts/build.sh
@@ -3,4 +3,4 @@
 export HOME=/root
 export DOTNET_CLI_HOME=/root
 dotnet publish --configuration Release AwsCodeDeployTest > /home/codedeploytest/logs.txt
-cp -r bin/Release/netcoreapp2.1/publish /home/codedeploytest
+cp -r AwsCodeDeployTest/bin/Release/netcoreapp2.1/publish /home/codedeploytest
