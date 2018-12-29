0b2c764fa4b6538f219b136e816b9535a3660bd5
雪あすか <tt@kmycode.net>
2018-12-29 09:37:27 +0900

    不要なシェルスクリプトを削除

--- cp-scripts/build.sh
+++
@@ -1,7 +0,0 @@
-﻿#!/bin/bash
-
-export HOME=/root
-export DOTNET_CLI_HOME=/root
-cd /home/codedeploytest/git/AwsCodeDeployTest
-dotnet publish --configuration Release > /home/codedeploytest/logs.txt
-cp -r /home/codedeploytest/git/AwsCodeDeployTest/bin/Release/netcoreapp2.1/publish /home/codedeploytest
